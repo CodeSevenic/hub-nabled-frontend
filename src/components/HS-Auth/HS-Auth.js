477bcc148ab2122657f2c33c7ed185eec0c3f6f5
CodeSevenic <sevenicbookings@gmail.com>
2023-04-12 14:14:26 +0200

    first commit

---
+++ src/components/HS-Auth/HS-Auth.js
@@ -0,0 +1,31 @@
+﻿import React, { useState } from 'react';
+import axios from 'axios';
+
+function HSAuth() {
+  const [loading, setLoading] = useState(false);
+
+  const handleAuthClick = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get('http://localhost:3000/api/v1/auth');
+      const authUrl = response.data.authUrl;
+      window.location.href = authUrl;
+    } catch (error) {
+      console.error('An error occurred:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>HubSpot OAuth Test</h1>
+      <button onClick={handleAuthClick} disabled={loading}>
+        {loading ? 'Loading...' : 'Authorize with HubSpot'}
+      </button>
+    </div>
+  );
+}
+
+export default HSAuth;
