4574144693ddbdc5f619677e3434233dd1ff37a8
CodeSevenic <sevenicbookings@gmail.com>
2023-04-13 10:45:47 +0200

    Hub-Nabled

---
+++ src/components/UserAppList/UserAppList.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+const UserAppList = ({ installedApps }) => {
+  return (
+    <div>
+      <h1>Your Installed Apps</h1>
+      <ul>
+        {installedApps.map((app) => (
+          <li key={app.id}>
+            <div>
+              <h3>{app.name}</h3>
+              <p>{app.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserAppList;
