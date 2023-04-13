c37fb63ce40e003cc18df61cd4668b85ba676399
CodeSevenic <sevenicbookings@gmail.com>
2023-04-13 10:42:02 +0200

    Hub-Nabled

---
+++ src/components/AppList/AppList.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+const AppList = ({ apps, onInstall }) => {
+  return (
+    <div>
+      <h1>Available Apps</h1>
+      <ul>
+        {apps.map((app) => (
+          <li key={app.id}>
+            <div>
+              <h3>{app.name}</h3>
+              <p>{app.description}</p>
+              <button onClick={() => onInstall(app.id)}>Install</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AppList;
