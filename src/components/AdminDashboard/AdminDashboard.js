d3eb42a606c2d678c7e7fc916268e575393c7618
CodeSevenic <sevenicbookings@gmail.com>
2023-05-12 11:54:07 +0200

    Hub-Nabled Code update

---
+++ src/components/AdminDashboard/AdminDashboard.js
@@ -0,0 +1,63 @@
+﻿// src/components/AdminDashboard.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const AdminDashboard = () => {
+  const [newApp, setNewApp] = useState({ appId: '', appSecret: '', scopes: '' });
+
+  // Update newApp state based on form input
+  const handleChange = (e) => {
+    setNewApp({ ...newApp, [e.target.name]: e.target.value });
+  };
+
+  // Submit new app's details
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:3001/apps', newApp);
+      setNewApp({ appId: '', appSecret: '', scopes: '' });
+      alert('App added successfully');
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred while adding the app');
+    }
+  };
+
+  return (
+    <div className="AdminDashboard">
+      <h2>Add New App</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="appId">App ID:</label>
+        <input
+          type="text"
+          id="appId"
+          name="appId"
+          value={newApp.appId}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="appSecret">App Secret:</label>
+        <input
+          type="text"
+          id="appSecret"
+          name="appSecret"
+          value={newApp.appSecret}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="scopes">Scopes (comma-separated):</label>
+        <input
+          type="text"
+          id="scopes"
+          name="scopes"
+          value={newApp.scopes}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Add App</button>
+      </form>
+    </div>
+  );
+};
+
+export default AdminDashboard;
