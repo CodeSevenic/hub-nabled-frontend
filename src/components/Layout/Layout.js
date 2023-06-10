f515cc876dfa6b48bb85b54c10f6e07e41396391
CodeSevenic <sevenicbookings@gmail.com>
2023-06-10 11:16:43 +0200

    Hub-Nabled

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,40 @@
 ﻿import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import './Layout.css';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('/logout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        console.log('Logout successful');
+        // Navigate to the login page after successful logout
+        navigate('/login');
+      } else {
+        console.log('Logout failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div>
       <header className="hub-nabled-header">
         <h1>Hub Nabled</h1>
         {/* Other elements for your header here */}
+        <button onClick={handleLogout} className="hbn-logout-btn">
+          Logout
+        </button>
       </header>
       <main>
         <Outlet />
